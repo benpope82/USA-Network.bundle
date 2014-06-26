2472cb773375be701391f49ade0d4c52a05f2fb9
sander1 <sander@plexapp.com>
2014-06-26 17:25:16 +0200

    first commit

---
+++ Contents/Code/__init__.py
@@ -0,0 +1,67 @@
+SHOWS_URL = 'http://tve-atcnbc.nbcuni.com/ep-live/2/usa/shows/iPad'
+EPISODES_URL = 'http://tve-atcnbc.nbcuni.com/ep-live/2/usa/container/x/iPad/%s'
+
+####################################################################################################
+def Start():
+
+	ObjectContainer.title1 = 'USA Network'
+	HTTP.CacheTime = CACHE_1HOUR
+	HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.117 Safari/537.36'
+
+####################################################################################################
+@handler('/video/usanetwork', 'USA Network')
+@route('/video/usanetwork/shows')
+def Shows():
+
+	oc = ObjectContainer()
+
+	if not Client.Platform in ('Android', 'iOS', 'Roku', 'Safari', 'Windows', 'Windows Phone'):
+		oc.header = 'Not supported'
+		oc.message = 'This channel is not supported on %s' % (Client.Platform if Client.Platform is not None else 'this client')
+		return oc
+
+	for show in JSON.ObjectFromURL(SHOWS_URL):
+		show_id = show['assetID']
+		title = show['title']
+		summary = show['description']
+		thumb = show['images'][0]['images']['show_tile']
+
+		oc.add(DirectoryObject(
+			key = Callback(Episodes, show_id=show_id, show=title),
+			title = title,
+			summary = summary,
+			thumb = Resource.ContentsOfURLWithFallback(thumb)
+		))
+
+	oc.objects.sort(key = lambda obj: obj.title)
+	return oc
+
+####################################################################################################
+@route('/video/usanetwork/episodes/{show_id}')
+def Episodes(show_id, show):
+
+	oc = ObjectContainer(title2=show)
+
+	for episode in JSON.ObjectFromURL(EPISODES_URL % show_id)['assetsX']:
+
+		if episode['type'] != 'video' or episode['subtype'] != 'episode' or episode['requiresAuth'] is not False:
+			continue
+
+		url = 'http://www.usanetwork.com/#%s|%s' % (show_id, episode['assetID'])
+
+		oc.add(EpisodeObject(
+			url = url,
+			show = show,
+			title = episode['title'],
+			summary = episode['description'],
+			thumb = Resource.ContentsOfURLWithFallback(episode['images'][0]['images']['episode_banner']),
+			season = int(episode['seasonNumber']),
+			index = int(episode['episodeNumber']),
+			duration = episode['totalDuration'],
+			originally_available_at = Datetime.FromTimestamp(episode['firstAiredDate'])
+		))
+
+	if len(oc) < 1:
+		return ObjectContainer(header='Empty', message='There are no episodes available for this show')
+
+	return oc
