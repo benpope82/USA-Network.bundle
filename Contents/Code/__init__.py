20548ba500baea7c1fbd5f87405bbe26842aae75
Sander <sander@plex.tv>
2016-08-20 17:51:42 +0200

    Update/rewrite channel code.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,64 +1,138 @@
-SHOWS_URL = 'http://tve-atcnbce.nbcuni.com/live/3/usa/containers/iPad'
-EPISODES_URL = 'http://tve-atcnbce.nbcuni.com/live/3/usa/containers/%s/iPad?filterBy=episode'
+NAME = 'USA Network'
+ART = 'art-default.jpg'
+ICON = 'icon-default.jpg'
+
+SHOWS_URL = 'http://tveatc-usa.nbcuni.com/awe3/live/5/usa/containers/iPadRetina'
+SECTIONS_URL = 'http://tveatc-usa.nbcuni.com/awe3/live/5/usa/asset/iPadRetina/%s'
+VIDEOS_URL = 'http://tveatc-usa.nbcuni.com/awe3/live/5/usa/containers/iPadRetina/%s?seasonNumber=%s&filterBy=%s'
 
 ####################################################################################################
 def Start():
 
-	ObjectContainer.title1 = 'USA Network'
+	ObjectContainer.title1 = NAME
 	HTTP.CacheTime = CACHE_1HOUR
 	HTTP.Headers['User-Agent'] = 'BRNetworking/2.7.0.1449 (iPad;iPhone OS-8.1)'
 
 ####################################################################################################
-@handler('/video/usanetwork', 'USA Network')
-@route('/video/usanetwork/shows')
+@handler('/video/usanetwork', NAME, art=ART, thumb=ICON)
 def Shows():
 
 	oc = ObjectContainer()
 
-	for show in JSON.ObjectFromURL(SHOWS_URL):
+	for show in JSON.ObjectFromURL(SHOWS_URL)['results']:
 
 		show_id = show['assetID']
 		title = show['title']
 		summary = show['description']
-		try: thumb = show['images'][0]['images']['show_tile']
-		except: thumb = ''
+		thumb = show['images'][0]['images']['show_thumbnail_16_by_9']
 
 		oc.add(DirectoryObject(
-			key = Callback(Episodes, show_id=show_id, show=title),
+			key = Callback(Sections, show_id=show_id, show=title),
 			title = title,
 			summary = summary,
-			thumb = Resource.ContentsOfURLWithFallback(thumb)
+			thumb = Resource.ContentsOfURLWithFallback(thumb, fallback=ICON)
 		))
 
-	oc.objects.sort(key = lambda obj: obj.title)
+	oc.objects.sort(key=lambda obj: Regex('^The ').split(obj.title)[-1])
 	return oc
 
 ####################################################################################################
-@route('/video/usanetwork/episodes/{show_id}')
-def Episodes(show_id, show):
+@route('/video/usanetwork/{show_id}/sections')
+def Sections(show_id, show):
+
+	oc = ObjectContainer(title2=show)
+	json_obj = JSON.ObjectFromURL(SECTIONS_URL % (show_id))
+	thumb = json_obj['images'][0]['images']['show_thumbnail_16_by_9']
+
+	has_episodes = False
+	has_clips = False
+
+	for season in json_obj['seasons']:
+
+		if season['hasEpisodes']:
+			has_episodes = True
+
+		if season['hasClips']:
+			has_clips = True
+
+	if has_episodes:
+
+		oc.add(DirectoryObject(
+			key = Callback(Seasons, show_id=show_id, show=show, filter_by='episode'),
+			title = 'Full Episodes',
+			thumb = Resource.ContentsOfURLWithFallback(thumb, fallback=ICON)
+		))
+
+	if has_clips:
+
+		oc.add(DirectoryObject(
+			key = Callback(Seasons, show_id=show_id, show=show, filter_by='clip'),
+			title = 'Clips',
+			thumb = Resource.ContentsOfURLWithFallback(thumb, fallback=ICON)
+		))
+
+	return oc
+
+####################################################################################################
+@route('/video/usanetwork/{show_id}/{filter_by}/seasons')
+def Seasons(show_id, show, filter_by):
+
+	oc = ObjectContainer(title2=show)
+	json_obj = JSON.ObjectFromURL(SECTIONS_URL % (show_id))
+	thumb = json_obj['images'][0]['images']['show_thumbnail_16_by_9']
+
+	for season in json_obj['seasons']:
+
+		if filter_by == 'episode' and season['hasEpisodes']:
+
+			oc.add(DirectoryObject(
+				key = Callback(Videos, show_id=show_id, show=show, filter_by='episode', season=season['number']),
+				title = 'Season %s' % (season['number']),
+				thumb = Resource.ContentsOfURLWithFallback(thumb, fallback=ICON)
+			))
+
+		elif filter_by == 'clip' and season['hasClips']:
+
+			oc.add(DirectoryObject(
+				key = Callback(Videos, show_id=show_id, show=show, filter_by='clip', season=season['number']),
+				title = 'Season %s' % (season['number']),
+				thumb = Resource.ContentsOfURLWithFallback(thumb, fallback=ICON)
+			))
+
+	return oc
+
+####################################################################################################
+@route('/video/usanetwork/{show_id}/{filter_by}/{season}/videos')
+def Videos(show_id, show, filter_by, season):
 
 	oc = ObjectContainer(title2=show)
 
-	for episode in JSON.ObjectFromURL(EPISODES_URL % show_id)['results']:
+	for episode in JSON.ObjectFromURL(VIDEOS_URL % (show_id, season, filter_by))['results']:
 
-		if episode['type'] != 'video' or episode['subtype'] != 'episode' or episode['requiresAuth']:
+		if episode['requiresAuth']:
 			continue
 
-		url = 'http://www.usanetwork.com/#%s|%s' % (show_id, episode['assetID'])
+		parent_id = episode['parentContainerId']
+		episode_id = episode['assetID']
+
+		title = episode['title']
+
+		try: ep_index = int(episode['episodeNumber'])
+		except: ep_index = None
+
+		try: thumb = episode['images'][0]['images']['video_detail_16_by_9']
+		except: thumb = ''
 
 		oc.add(EpisodeObject(
-			url = url,
+			url = 'usanetwork://%s/%s' % (parent_id, episode_id),
 			show = show,
-			title = episode['title'],
+			title = title,
 			summary = episode['description'],
-			thumb = Resource.ContentsOfURLWithFallback(episode['images'][0]['images']['episode_banner']) if 'episode_banner' in episode['images'][0]['images'] else '',
+			thumb = Resource.ContentsOfURLWithFallback(url=thumb),
 			season = int(episode['seasonNumber']),
-			index = int(episode['episodeNumber']) if 'episodeNumber' in episode else None,
+			index = ep_index,
 			duration = episode['totalDuration'],
 			originally_available_at = Datetime.FromTimestamp(episode['firstAiredDate'])
 		))
-
-	if len(oc) < 1:
-		return ObjectContainer(header='Empty', message='There are no episodes available for this show')
 
 	return oc
